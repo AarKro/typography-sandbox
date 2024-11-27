3fca5a81c654709051ec530167d1397a5af8d55a
Aaron Kromer <kromer.aaron@gmail.com>
2024-11-27 18:33:32 +0100

    update font weight request

--- src/components/FontHandler/FontHandler.tsx
+++ src/components/FontHandler/FontHandler.tsx
@@ -9,9 +9,9 @@ export const FontHandler: FC = () => {
 
   return (
     <Helmet>
-      <link rel="stylesheet" href={`https://fonts.googleapis.com/css?family=${masterConfig.fontFamily}`}/>
+      <link rel="stylesheet" href={`https://fonts.googleapis.com/css?family=${masterConfig.fontFamily}:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900`}/>
       {Object.entries(fontDisplays).map(([id, config]) => (
-        <link key={id} rel="stylesheet" href={`https://fonts.googleapis.com/css?family=${config.fontConfig.fontFamily}`}/>
+        <link key={id} rel="stylesheet" href={`https://fonts.googleapis.com/css?family=${config.fontConfig.fontFamily}:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900`}/>
       ))}
     </Helmet>
   );
